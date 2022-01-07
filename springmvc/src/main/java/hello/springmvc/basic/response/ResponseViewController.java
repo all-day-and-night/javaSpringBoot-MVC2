26d35147309c41a23261ac7ff2f9973482e09de9
all-day-and-night <jwug0821@gmail.com>
2022-01-07 14:11:29 +0900

    Controller ofr Response View

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,11 @@
+package hello.springmvc.basic.response;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/")
+}
